6db8bac8b30e63ccb09e37bae4e0e2979b4b628d
riabchun <riabchun.andrii@gmail.com>
2021-01-02 19:23:10 +0200

    sticker changes

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import telebot
 from PIL import Image
 import os
-from config import * #config file with Bot token and owner id
+from config import * #create config.py file next to this one. config.py example copy from https://github.com/BobVolskiy/adding-to-your-stickerpack/blob/main/README.md
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id,'✌ Отправь мне фото или стикер, чтобы начать...')
+@bot.message_handler(commands=["help"])
 def help(message):
-    bot.send_message(message.chat.id,'Этот бот добавляет любое отправленное фото или стикер в стикерпак\nПользоваться ботом невозможно, если вы не Боб\n\n'+str(stickerlink)+'\n\nБота создал @bob_volskiy\nИсходный код на Github: github.com/BobVolskiy/adding-to-your-stickerpack')
+    bot.send_message(message.chat.id,'🤖 Этот бот добавляет любое отправленное фото или стикер в стикерпак Боба\nПользоваться ботом невозможно, если вы не Боб\n\n'+str(stickerlink)+'\n\n👨‍💻Бота создал @bob_volskiy\ngithub.com/BobVolskiy/adding-to-your-stickerpack')
 
 def resizing(im):
     width, height = im.size
@@ -28,28 +31,28 @@ def converting(fileID,CHATID):
         b = bytearray(f)
     try:
         bot.add_sticker_to_set(owner,'bv_by_bvsticker_bot',b,'😘')
-        bot.send_message(CHATID,'Добавил в стикерпак изображение выше \n'+stickerlink)
+        bot.send_message(CHATID,'✅ Добавил в стикерпак изображение выше \n'+stickerlink)
     except:
-        bot.send_message(CHATID,'Что-то пошло не так...')
+        bot.send_message(CHATID,'😫 Что-то пошло не так...')
     os.remove("resized.png")
     os.remove("image.jpg")
 
 @bot.message_handler(content_types=["photo"])
 def photo(message):
+    bot.send_message(message.chat.id,'🔁 Принял фотку. Конвертирую и добавляю...')
     if message.chat.id==owner:
-        bot.send_message(message.chat.id,'Принял фотку. Конвертирую и добавляю...')
         fileID = message.photo[-1].file_id
         converting(fileID,message.chat.id)
     else: 
-        bot.send_message(message.chat.id,'Стоп, ты не Боб. Пиздуй лесом')
+        bot.send_message(message.chat.id,'Стоп, ты не Боб 😆')
 
 @bot.message_handler(content_types=["sticker"])
 def sticker(message):
+    bot.send_message(message.chat.id,'🔁 Принял стикер. Конвертирую и добавляю...')
     if message.chat.id==owner:
-        bot.send_message(message.chat.id,'Принял стикер. Конвертирую и добавляю...')
         fileID = message.sticker.file_id
         converting(fileID,message.chat.id)
     else: 
-        bot.send_message(message.chat.id,'Стоп, ты не Боб. Пиздуй лесом')
-print('Бот начал свою работу')
+        bot.send_message(message.chat.id,'Стоп, ты не Боб 😆')
+print('Бот начал свою работу...')
 bot.polling()
